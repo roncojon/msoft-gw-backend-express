a5f97d30afe72aca94593f493fb5604fb603d14d
RonaldGS <rguillen@datamart.cl>
2024-02-18 12:44:40 -0500

    organize files

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-const routes = require('./src/routes/routes');
+const routes = require('./routes/routes');
 
 app.use('/', routes);
 
